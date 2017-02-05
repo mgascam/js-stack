ca5322c4dbe0d16b3a4e3b8341f7ea722d54368b
Miguel Gasca <miguelgasca@gmail.com>
2017-02-05 14:26:17 +0100

    Second

--- src/dog.js
+++ src/dog.js
@@ -1,3 +1,14 @@
 /**
  * Created by miguel on 5/02/17.
  */
+class Dog {
+    constructor(name) {
+        this.name = name;
+    }
+
+    bark() {
+        return `Wah wah, I am ${this.name}`;
+    }
+}
+
+module.exports = Dog;
