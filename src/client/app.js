ffa3a09035e3906fda9b352e39e77cfae3d6b71e
Miguel Gasca <miguelgasca@gmail.com>
2017-02-05 20:51:18 +0100

    webpack

--- src/client/app.js
+++ src/client/app.js
@@ -1,3 +1,9 @@
 /**
  * Created by miguel on 5/02/17.
  */
+// import 'babel-polyfill';
+import Dog from '../shared/dog';
+
+const browserToby = new Dog('Browser Toby');
+
+document.querySelector('.app').innerText = browserToby.bark();
